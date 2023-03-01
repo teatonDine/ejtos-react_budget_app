4860a4e469399fde20c383a85a7e29a1a2bb04e8
Tonya Eaton <tlharrison79@gmail.com>
2023-02-28 23:12:33 -0500

    Stuck on Task 2

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,18 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const [b, setCost] = useState('');
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <div>Budget: £
+                <input type='number' 
+                        step='10' 
+                        id='b'
+                        max='20000' 
+                        value={budget}
+                        onChange={(event) => setCost(event.target.value)}>
+                    </input></div>
         </div>
     );
 };
